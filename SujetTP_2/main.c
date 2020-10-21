acaaf3f8527aedffa7f79df8f8a1e30d354a45e1
OonnaC <35567072+OonnaC@users.noreply.github.com>
2020-10-21 11:57:28 +0200

    Création du TP 2
    version 1

---
+++ SujetTP_2/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(char src[], char code[])
+{
+
+    printf("message original:\n");
+    scanf("%s \n",src);
+    int i, j;
+    while (j)
+    for (i=0;i<3;i++)
+    return 0;
+}
