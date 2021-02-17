886bd22c29afa338e2afc30ab974669fa576a4f6
OonnaC <35567072+OonnaC@users.noreply.github.com>
2021-02-17 14:45:26 +0100

    Procédure affiche_prefixe

--- AlgosTests/TP_ABR_CodeBlocks/main.c
+++ AlgosTests/TP_ABR_CodeBlocks/main.c
@@ -11,21 +11,33 @@ ABR ajout_f(Info v, ABR A)
         return creer_feuilleABR(v);
 
     }else{
-        if (v <=S valracABR(A)){
+        if (v <= valracABR(A)){
                 A-> g = ajout_f(v, gaucheABR(A));
 
         }else{
             A-> d = ajout_f(v, droitABR(A));
-S
+
         }
         return A;
     }
 }
 
+ABR affiche_prefixe(ABR A)
+{
+    if (est_videABR (A)){
+
+            return;
+
+    }else{
+        printf("%d",valracABR(A));
+        affiche_prefixe(gaucheABR(A));
+        affiche_prefixe(droitABR(A));
+    }
+}
 
 int main()
 {
     //chaine = ajout_f(10,);
-    printf(chaine);
+    //printf(chaine);
     return 0;
 }
