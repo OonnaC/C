f171a0a0ca658625bdcc9dbc777c26850a03c82f
OonnaC <35567072+OonnaC@users.noreply.github.com>
2021-02-17 15:15:39 +0100

    Procédures
    -Modification de affiche_prefixeABR
    -Ajout de affiche_postfixeABR et affiche_infixeABR

--- AlgosTests/TP_ABR_CodeBlocks/main.c
+++ AlgosTests/TP_ABR_CodeBlocks/main.c
@@ -22,7 +22,7 @@ ABR ajout_f(Info v, ABR A)
     }
 }
 
-ABR affiche_prefixe(ABR A)
+void affiche_prefixeABR(ABR A)
 {
     if (est_videABR (A)){
 
@@ -30,14 +30,81 @@ ABR affiche_prefixe(ABR A)
 
     }else{
         printf("%d",valracABR(A));
-        affiche_prefixe(gaucheABR(A));
-        affiche_prefixe(droitABR(A));
+        if (!est_videABR(gaucheABR(A))){
+            putchar('(');
+            affiche_prefixeABR(gaucheABR(A));
+            putchar(')');
+        }
+        if (!est_videABR(droitABR(A))){
+            putchar('(');
+            affiche_prefixeABR(droitABR(A));
+            putchar(')');
+        }
+    }
+}
+
+void affiche_infixeABR(ABR A)
+{
+    if (est_videABR (A)){
+
+            return;
+
+    }else{
+
+        if (!est_videABR(gaucheABR(A))){
+            putchar('(');
+            affiche_infixeABR(gaucheABR(A));
+            putchar(')');
+        }
+        printf("%d",valracABR(A));
+
+        if (!est_videABR(droitABR(A))){
+            putchar('(');
+            affiche_infixeABR(droitABR(A));
+            putchar(')');
+        }
+    }
+}
+
+void affiche_postfixeABR(ABR A)
+{
+    if (est_videABR (A)){
+
+            return;
+
+    }else{
+
+        if (!est_videABR(gaucheABR(A))){
+            putchar('(');
+            affiche_postfixeABR(gaucheABR(A));
+            putchar(')');
+        }
+
+        if (!est_videABR(droitABR(A))){
+            putchar('(');
+            affiche_postfixeABR(droitABR(A));
+            putchar(')');
+        }
+
+        printf("%d",valracABR(A));
     }
 }
 
 int main()
 {
-    //chaine = ajout_f(10,);
-    //printf(chaine);
+    ABR A = NULL;
+    A = ajout_f(10, A);
+    A = ajout_f(7, A);
+    A = ajout_f(20, A);
+    A = ajout_f(3, A);
+    A = ajout_f(8, A);
+    A = ajout_f(15, A);
+    A = ajout_f(30, A);
+    affiche_prefixeABR(A);
+    printf("\n");
+    affiche_infixeABR(A);
+    printf("\n");
+    affiche_postfixeABR(A);
+
     return 0;
 }
